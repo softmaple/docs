de92a23e8d40798defea25ce4bdf37affc4a6cb9
Yadong Zhang <zhyd007@gmail.com>
2021-12-23 15:48:38 +0800

    chore: fix copyings.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: insightsPageUrl,
-            label: 'Insights (beta)',
+            label: 'Insights (demo)',
             position: 'left',
           },
           {
